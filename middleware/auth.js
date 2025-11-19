989ed3159614d9bfaf629041d5e5214ba84a6684
touseeqiqbal <81242137+touseeqiqbal@users.noreply.github.com>
2025-11-19 00:40:25 +0000

    fix pem keys

--- middleware/auth.js
+++ middleware/auth.js
@@ -6,7 +6,8 @@ try {
   if (process.env.FIREBASE_SERVICE_ACCOUNT) {
     const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
     // FIX private_key line breaks
-  serviceAccount.private_key = serviceAccount.private_key.replace(/\\n/g, '\n');
+  serviceAccount.private_key = serviceAccount.private_key.replace(/\\n/g, "\n");
+  
     admin.initializeApp({
       credential: admin.credential.cert(serviceAccount)
     });

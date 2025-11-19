bfa86d9d3999064981eb57f10f21380632508b19
touseeqiqbal <81242137+touseeqiqbal@users.noreply.github.com>
2025-11-19 00:21:45 +0000

    final firebase storage fix

--- middleware/auth.js
+++ middleware/auth.js
@@ -5,6 +5,7 @@ let firebaseInitialized = false;
 try {
   if (process.env.FIREBASE_SERVICE_ACCOUNT) {
     const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+    serviceAccount.private_key = serviceAccount.private_key.replace(/\\n/g, "\n");
     admin.initializeApp({
       credential: admin.credential.cert(serviceAccount)
     });
